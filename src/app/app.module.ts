9dbb66da99c7826b4a55f232cd2f4dafd033705d
Grigori Hlopkov <grigori.hlopkov@gmail.com>
2020-07-01 22:31:54 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { SelectorComponent } from './selector/selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    OrganizerComponent,
+    SelectorComponent
   ],
   imports: [
     BrowserModule
